3bb2c8ccb47e150c6ed6c6d7591df65a7abb5a6e
Mamun Sourav <souravcuetcse2010@gmail.com>
2017-07-12 20:42:48 -0700

    Logging functionality added using log4j

--- CustomerManagement/src/main/java/com/banglaESS/controller/CustomerController.java
+++ CustomerManagement/src/main/java/com/banglaESS/controller/CustomerController.java
@@ -4,28 +4,34 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.apache.log4j.Logger;
 
 import com.banglaESS.model.CustomerObjectForm;
 
 @Controller
 public class CustomerController {
+	private static final Logger logger = Logger.getLogger(CustomerController.class);
 
 	@RequestMapping(value = "dashboard")
 	public String getDashboard(@ModelAttribute("customerObject") CustomerObjectForm customerObject, Model model) {
-		System.out.println("Check dashboard");
+		//System.out.println("Check dashboard");
+		logger.debug("Checking dashboard");
 		return "customer";
 	}
 	
 	
 	@RequestMapping(value = "addCustomer")
 	public String addCustomer(@ModelAttribute("customerObject") CustomerObjectForm customerObject, Model model) {
-		System.out.println("Check add customer");
+		//System.out.println("Check add customer");
+		logger.debug("Checking addCustomer page");
 		model.addAttribute("customerObject", customerObject);
 		return "addCustomer";
 	}
 	@RequestMapping(value = "showCustomer")
 	public String showCustomer(@ModelAttribute("customerObject") CustomerObjectForm customerObject, Model model) {
-		System.out.println("Check show customer");
+		//System.out.println("Check show customer");
+		logger.debug("Checking show customer page");
+		logger.info("Name:"+customerObject.getName());
 		model.addAttribute("customerObject", customerObject);
 		return "showCustomer";
 	}
